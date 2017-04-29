0dd601223b7cf070a8f2b40d73fd0d5414d06f32
David Owens II <david@owensd.io>
2017-04-28 21:03:08 -0700

    Moving out the language server to its own repo. Removed the snippets that didn't import correctly. Added some error handling to the loading of the language service tool. Added configuration block information for bug tagging and language service.

---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Kiad Studios, LLC. All rights reserved.
+ * Licensed under the MIT License. See License in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { window, languages, workspace, Uri, Range, Disposable, ExtensionContext, TextDocument, CancellationToken, DocumentLink } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
+
+let languageServerId = 'swift-langsrv';
+
+function registerSwiftBugLinkProvider(context: ExtensionContext) {
+	// Provides easy access to Swift bugs.
+	// example:
+	// SwiftBug(SR-2688)
+	let disposableSwiftBugLinkProvider = languages.registerDocumentLinkProvider('swift', {
+		provideDocumentLinks: function(document: TextDocument, token: CancellationToken): DocumentLink[] {
+			let links: DocumentLink[] = [];
+			let re = /(SwiftBug\((SR-\d+)\))/g;
+			let documentText = document.getText();
+			
+			var match
+			while ((match = re.exec(documentText)) != null) {
+				let pos = document.positionAt(match.index);
+				let range = new Range(pos, pos.translate(0, match[0].length + 1));
+				if (match[1]) {
+					let uri = Uri.parse('https://bugs.swift.org/browse/' + match[2]);
+					links.push(new DocumentLink(range, uri));
+				}
+			}
+			return links;
+		}
+	});
+	context.subscriptions.push(disposableSwiftBugLinkProvider);
+}
+
+// Launches the Swift Language Service tool.
+function registerSwiftLanguageService(context: ExtensionContext) {
+	let config = workspace.getConfiguration(languageServerId);
+	let langsrvPath = config.get('languageServicePath', './lib/macos/swift-langsrv');
+	let debugOptions = ["--nolazy", "--debug=6009"];
+
+	fs.exists(langsrvPath, (exists: boolean) => {
+		if (exists) {
+			let serverOptions: ServerOptions = {
+				run : { command: langsrvPath },
+				debug: { command: langsrvPath, args: debugOptions }
+			}
+			
+			let clientOptions: LanguageClientOptions = {
+				documentSelector: ['swift'],
+				synchronize: {
+					configurationSection: languageServerId,
+					fileEvents: workspace.createFileSystemWatcher('**/.swift')
+				}
+			}
+			
+			let swiftLanguageService = new LanguageClient(languageServerId, 'Swift Language Service', serverOptions, clientOptions);
+			context.subscriptions.push(swiftLanguageService.start());
+		}
+		else {
+			window.showErrorMessage('There was no language service tool at the given path: ' + langsrvPath);
+		}
+	});
+}
+
+export function activate(context: ExtensionContext) {
+	let config = workspace.getConfiguration(languageServerId);
+
+	let enableBugLinks = config.get('enableSwiftBugLinks', true);
+	if (enableBugLinks) { registerSwiftBugLinkProvider(context); }
+
+	let enableLanguageService = config.get('enableLanguageService', true);
+	if (enableLanguageService) { registerSwiftLanguageService(context); }
+}
